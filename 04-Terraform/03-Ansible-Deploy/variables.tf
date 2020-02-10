c6cea206546a6cde12e8cd83b716e451b6c1e33a
Laharivegiraju22 <58691151+Laharivegiraju22@users.noreply.github.com>
2020-02-10 09:52:38 +0530

    Update variables.tf

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -6,10 +6,6 @@ variable "pvt_key" {
   default = "/root/.ssh/hkey.pem"
 }
 
-variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
-}
-
 variable "sg-id" {
   default = "sg-00bf8e15b47e23b8d"
 }
