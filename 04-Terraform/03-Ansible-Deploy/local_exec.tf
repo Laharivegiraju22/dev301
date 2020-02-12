d7ce29ff70abff892db926afe71c904f99d54704
Laharivegiraju22 <58691151+Laharivegiraju22@users.noreply.github.com>
2020-02-12 11:07:06 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -24,8 +24,10 @@ provisioner "local-exec" {
   command = <<EOT
         sleep 100;
         > jenkins-ci.ini;
-       export ANSIBLE_HOST_KEY_CHECKING=False;
-       ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible/04-Tomcat/web-playbook.yaml -u ubuntu -v
+        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+        export ANSIBLE_HOST_KEY_CHECKING=False;
+        echo "${var.host-ip}" | tee -a jenkins-ci.ini;
+        ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible/04-Tomcat/web-playbook.yaml -u ubuntu -v
     EOT
 }
 }
