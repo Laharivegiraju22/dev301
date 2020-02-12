718cda45410103ea7aae86df814640f52fcaa802
Laharivegiraju22 <58691151+Laharivegiraju22@users.noreply.github.com>
2020-02-12 11:38:17 +0530

    Update variables.tf

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -6,18 +6,16 @@ variable "pvt_key" {
   default = "/root/.ssh/hkey.pem"
 }
 
-variable "ami_id"{
-  default = "ami-02d0ea44ae3fe9561"
+variable "amis" {
+  type = "map"
+  default = {
+   
+    "us-west-2" = "ami-02d0ea44ae3fe9561"
   }
-
+}
 variable "instance_type"{
   default = "t2.micro"
   }
-
-variable "region"{
-  default = "us-west-2"
-  }
-
 variable "sg-id" {
   default = "sg-00bf8e15b47e23b8d"
 }
