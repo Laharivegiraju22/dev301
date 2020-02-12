69f6e96dcd3edbad457db27f8a4416da0d19a197
Laharivegiraju22 <58691151+Laharivegiraju22@users.noreply.github.com>
2020-02-12 10:25:11 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -2,9 +2,7 @@ terraform {
   backend "local" {
     path = "/tmp/terraform/workspace/terraform.tfstate"
   }
-
 }
-
 provider "aws" {
   region = "${var.region}"
 
@@ -15,22 +13,4 @@ resource "aws_instance" "backend" {
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
-
-}
-
-resource "null_resource" "remote-exec-1" {
-    connection {
-    user        = "ubuntu"
-    type        = "ssh"
-    private_key = "${file(var.pvt_key)}"
-    host        = "${aws_instance.backend.public_ip}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install python sshpass -y",
-    ]
-  }
-}
 }
